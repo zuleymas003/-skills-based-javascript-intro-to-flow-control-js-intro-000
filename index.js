937f51cef53936ff94284a51a357a509ea139b1d
zuleymas003 <zuleymas27684@students.laalliance.org>
2016-11-02 16:14:40 +0000

    First saved version of my nitrous code.

---
+++ index.js
@@ -0,0 +1,25 @@
+// write your code below!
+
+function basicTeenager (age) {
+if(age >= 13 && age <= 19){
+ return "You are a teenager!"
+}else {
+ return undefined
+}
+};
+
+function teenager(age) {
+
+}
+
+function ageChecker(age) {
+
+}
+
+function ternaryTeenager(age) {
+
+}
+
+function switchAge(age) {
+
+}
